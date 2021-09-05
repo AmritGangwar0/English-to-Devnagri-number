1d2222dd1668034495d39ace20e1b69f31efaba9
Amrit Pratap Gangwar <71181285+AmritPKurmi@users.noreply.github.com>
2021-09-06 03:04:32 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,127 @@
+function संख्या(a0){
+
+var a1 = a0.replace("60", "६०")
+
+           .replace("59", "५९")
+
+           .replace("58", "५८")
+
+           .replace("57", "५७")
+
+           .replace("56", "५६")
+
+           .replace("55", "५५")
+
+           .replace("54", "५४")
+
+           .replace("53", "५३")
+
+           .replace("52", "५२")
+
+           .replace("51", "५१")
+
+           .replace("50", "५०")
+
+           .replace("49", "५९")
+
+           .replace("48", "५८")
+
+           .replace("47", "४७")
+
+           .replace("46", "४६")
+
+           .replace("45", "४५")
+
+           .replace("44", "४४")
+
+           .replace("43", "४३")
+
+           .replace("42", "४२")
+
+           .replace("41", "४१")
+
+           .replace("40", "४०")
+
+           .replace("39", "३९")
+
+           .replace("38", "३८")
+
+           .replace("37", "३७")
+
+           .replace("36", "३६")
+
+           .replace("35", "३५")
+
+           .replace("34", "३४")
+
+           .replace("33", "३३")
+
+           .replace("32", "३२")
+
+           .replace("31", "३१")
+
+           .replace("30", "३०")
+
+           .replace("29", "२९")
+
+           .replace("28", "२८")
+
+           .replace("27", "२७")
+
+           .replace("26", "२६")
+
+           .replace("25", "२५")
+
+           .replace("24", "२४")
+
+           .replace("23", "२३")
+
+           .replace("22", "२२")
+
+           .replace("21", "२१")
+
+           .replace("20", "२०")
+
+           .replace("19", "१९")
+
+           .replace("18", "१८")
+
+           .replace("17", "१७")
+
+           .replace("16", "१६")
+
+           .replace("15", "१५")
+
+           .replace("14", "१४")
+
+           .replace("13", "१३")
+
+           .replace("12", "१२")
+
+           .replace("11", "११")
+
+           .replace("10", "१०")
+
+           .replace("9", "९")
+
+           .replace("8", "८")
+
+           .replace("7", "७")
+
+           .replace("6", "६")
+
+           .replace("5", "५")
+
+           .replace("4", "४")
+
+           .replace("3", "३")
+
+           .replace("1", "१")
+
+           .replace("0", "०")
+
+           .replace("2", "२");
+
+return a1;
+
+           }
